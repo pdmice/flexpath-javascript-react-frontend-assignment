1ea10ed049ba448a394c62fed64a8fb2d8c3bab1
pdmice <25230978+pdmice@users.noreply.github.com>
2025-03-13 13:21:24 -0400

    Start adding keyword search

--- assignment-app/src/components/Search.jsx
+++ assignment-app/src/components/Search.jsx
@@ -11,11 +11,17 @@ export default function Search() {
     
   }
 
+  const handleKeyword = (e) => {
+    console.log(e.target.value)
+    setKeyword(e.target.value)
+  }
+
   
 
   return (
     <>
     {console.log("category state is: ", category)}
+    {console.log("keyword is: ", keyword)}
     <div className="container">
      <form >
       <label className="form-label" for="category">Select data point to filter by</label>
@@ -25,7 +31,13 @@ export default function Search() {
         onChange={(e) => handleCategory(e)}>
         {categories.map((i) =>  <option value={i}>{i}</option>)}
       </select>
-      <input type="text" className="form-control" placeholder="Search by keyword"></input>
+      <input 
+        type="text" 
+        className="form-control" 
+        placeholder="Search by keyword"
+        onChange={(e) => handleKeyword(e)}
+        ></input>
+        
 
       
     </form>
